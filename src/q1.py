289b31482d47e2ce4c6d8417e699ca8bba984b3c
Senyo Ohene <senohene@gmail.com>
2023-04-23 16:05:58 -0400

    did more work on q3, q4 and q5

--- src/q1.py
+++ src/q1.py
@@ -6,35 +6,25 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 df = load_diabetes(as_frame=True)
-#print(df.data['age'])
-#print(df.feature_names)
 
 r_squared_values = {}
 
-# for item in df.feature_names:
-#     model = sm.OLS(df.target,df.data[item])
-#     results = model.fit()
-#     r_squared_values[item] = results.rsquared
-
 for item in df.feature_names:
     x = df.data[item].values.reshape(-1,1)
-    #print(x.shape)
     y = df.target.values.reshape(-1,1)
-    #print(y.shape)
     model = LinearRegression()
     results = model.fit(x,y)
     r_squared_values[item] = results.score(x,y)
 
-sorted_r_squared_values = dict(sorted(r_squared_values.items(), key=lambda x:x[1],reverse=True))
+r_squared_values = dict(sorted(r_squared_values.items(), key=lambda x:x[1],reverse=True))
 
-#print(sorted_r_squared_values)
-
-plt.bar(range(len(sorted_r_squared_values)), 
-list(sorted_r_squared_values.values()), tick_label=list(sorted_r_squared_values.keys()))
+plt.bar(range(len(r_squared_values)), 
+list(r_squared_values.values()), tick_label=list(r_squared_values.keys()))
 plt.title("Features by $R^{2}$")
 plt.xlabel("Features")
 plt.ylabel("$R^{2}$")
 plt.savefig("figs/features_by_r_squared")
+plt.close()
 #plt.show()
 
 # plt.scatter(df.data['bmi'], df.target)
--- src/q2.py
+++ src/q2.py
@@ -7,8 +7,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import SequentialFeatureSelector
 
-from q1 import df
+from q1 import df, r_squared_values
 
-selector = SequentialFeatureSelector(estimator=LinearRegression())
-selector.fit(df)
-print(selector.get_support())
+sequential_features = []
+
+for i in range(len(df.feature_names)-1):
+    selector = SequentialFeatureSelector(estimator=LinearRegression(), n_features_to_select=i+1)
+    selector.fit(df.data, df.target)
+    for item in selector.get_feature_names_out():
+        if item not in sequential_features:
+            sequential_features.append(item)
+
+for item in df.feature_names:
+    if item not in sequential_features:
+        sequential_features.append(item)
+
+plt.bar(x = sequential_features, height=[r_squared_values[feature] for feature in sequential_features])
+plt.title("Features by $R^{2}$")
+plt.xlabel("Features")
+plt.ylabel("$R^{2}$")
+plt.savefig("figs/sfs_features_by_r_squared")
+plt.close()
